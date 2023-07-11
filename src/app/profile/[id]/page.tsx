5c433eecb571cedb5379b73b6f5392c587d339fd
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2023-07-11 12:20:24 +0300

    first commit

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserProfile = ({params}: any) => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <h1>Profile</h1>
+        <hr/>
+        <p>Profile page 
+            <span>{params.id}</span></p>
+    </div>
+  )
+}
+
+export default UserProfile
